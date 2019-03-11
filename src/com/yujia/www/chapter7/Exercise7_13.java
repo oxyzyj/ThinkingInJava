090dc50bc7fdcb9a64f99e1f3e6cd69d68bcc9c1
Yujia Zhai <oxyzyj@YujiadeMacBook-Air.local>
2019-03-10 21:37:23 -0400

    7-13

--- src/com/yujia/www/chapter7/Exercise7_13.java
+++ src/com/yujia/www/chapter7/Exercise7_13.java
@@ -9,16 +9,28 @@ public class Exercise7_13 {
     TWENTY,
     HUNDRED,
   }
-  public static void main(String[] args) {
-    for(Currency c : Currency.values()) {
-      switch (c) {
-        case ONE: System.out.println("ONE"); break;
-        case TWO: System.out.println("TWO"); break;
-        case FIVE: System.out.println("FIVE"); break;
-        case TEN: System.out.println("TEN"); break;
-        case TWENTY: System.out.println("TWENTY"); break;
-        default: System.out.println("HUNDRED");
-      }
+  public static void main(final String[] args) {
+    for(final Currency c : Currency.values()) {
+      System.out.println(c.name());
+//      switch (c) {
+//        case ONE:
+//          System.out.println("ONE");
+//          break;
+//        case TWO:
+//          System.out.println("TWO");
+//          break;
+//        case FIVE:
+//          System.out.println("FIVE");
+//          break;
+//        case TEN:
+//          System.out.println("TEN");
+//          break;
+//        case TWENTY:
+//          System.out.println("TWENTY");
+//          break;
+//        default:
+//          System.out.println("HUNDRED");
+//      }
     }
   }
 }
