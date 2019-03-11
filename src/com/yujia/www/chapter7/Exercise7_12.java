d557536a1b0dbc59cc5385cb26318e7d9ea2cf68
Yujia Zhai <oxyzyj@YujiadeMacBook-Air.local>
2019-03-10 21:30:05 -0400

    7-12

--- src/com/yujia/www/chapter7/Exercise7_12.java
+++ src/com/yujia/www/chapter7/Exercise7_12.java
@@ -1,31 +1,37 @@
 package com.yujia.www.chapter7;
 
 class Tank{
-  boolean isFilled = false;
+  private boolean _isFilled = false;
+
   Tank() {
     this(false);
   }
+
   Tank(boolean isFilled) {
-    this.isFilled = isFilled;
+    this._isFilled = isFilled;
   }
+
   void Fill() {
-    isFilled = true;
+    _isFilled = true;
   }
+
   void Empty() {
-    isFilled = false;
+    _isFilled = false;
   }
+
+  @Override
   protected void finalize() {
-    if (isFilled) {
-      System.out.println("Err: the tank is still be filled");
+    if (_isFilled) {
+      System.err.println("Err: the tank is still be filled");
     }
   }
 }
 
 public class Exercise7_12 {
-  public static void main(String[] args) {
-    Tank t1 = new Tank();
+  public static void main(final String[] args) {
+    final Tank t1 = new Tank();
     t1.Fill();
-    Tank t2 = new Tank();
+    final Tank t2 = new Tank();
     t2.Fill();
     t2.Empty();
     new Tank(true);  // drop the reference, forget to clean up
