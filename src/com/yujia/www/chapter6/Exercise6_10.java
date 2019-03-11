8f90b6ae4addd9d32b8eb2c44c80a99ce8f9b31e
Yujia Zhai <oxyzyj@YujiadeMacBook-Air.local>
2019-03-10 21:17:54 -0400

    6_10

--- src/com/yujia/www/chapter6/Exercise6_10.java
+++ src/com/yujia/www/chapter6/Exercise6_10.java
@@ -2,23 +2,24 @@ package com.yujia.www.chapter6;
 
 import java.util.Arrays;
 import java.util.HashSet;
+import java.util.Set;
 
 public class Exercise6_10 {
 
-  public static void main(String[] args) {
-    HashSet<Integer> result = new HashSet<>();
+  public static void main(final String[] args) {
+    final Set<Integer> result = new HashSet<>();
     for (int x = 10; x <= 99; x++) {
       for (int y = 10; y <= x; y++) {
-        int z = x * y;
-        char a[] = Integer.toString(z).toCharArray();
-        char b[] = (Integer.toString(x) + Integer.toString(y)).toCharArray();
+        final int z = x * y;
+        final char a[] = Integer.toString(z).toCharArray();
+        final char b[] = (Integer.toString(x) + Integer.toString(y)).toCharArray();
         Arrays.sort(a);
         Arrays.sort(b);
         if (Arrays.equals(a, b))
           result.add(z);
       }
     }
-    for (int i : result)
+    for (final int i : result)
       System.out.println(i);
   }
 }
